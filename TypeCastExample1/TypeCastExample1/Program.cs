56f781fbc1598a8ecec3550d4204a9c4acd98d71
kimkt <whitepink1@naver.com>
2019-02-09 13:12:47 +0900

    TypeCastExample 수업내용

---
+++ TypeCastExample1/TypeCastExample1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace TypeCastExample1
+{
+	class Program
+	{
+		public static void Main() //(string[] args)
+		{
+			short a = 10;
+			int b = a;
+			int c = 50000;
+			//short d = (short)c;
+			//Console.WriteLine("Short:{0}", d);
+			//------------------------------------
+			try
+			{
+				short d = checked((short)c);
+				Console.WriteLine("Short:{0}", d);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine("\n");
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+	}
+}
