1ebe6f518b35410775af4d56d50dee8d5ce49e16
kimkt <whitepink1@naver.com>
2019-02-09 12:45:53 +0900

    시작 : 수업예제1 Helow world

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "김기태";
+			int k = 40;
+
+			Console.WriteLine($"Hellow {name}");
+			Console.WriteLine("Hellow " + name);
+			//Console.WriteLine("Hellow " + name +" "+ name);
+			Console.WriteLine("Hellow {0} {1}", name, k);
+		}
+	}
+}
